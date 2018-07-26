7768c54379eab2dc0d86ce5bead168b3ef18d700
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-07-26 18:05:14 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,4 +8,14 @@ class App < Sinatra::Base
 
   get '/new' do
     erb :create_puppy
+  end
+
+  post '/display_puppy' do
+    @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+    
+    erb :display_puppy
+  end
+  
 end
+
+
